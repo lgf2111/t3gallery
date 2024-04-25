c9e44be4e94eeb433c9522d6a11229e16074f687
Jeff Lee <lgf2111@gmail.com>
2024-04-26 00:27:40 +0800

    analytics on delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,46 +1,56 @@
-import "server-only"
-import { db } from "./db"
+import "server-only";
+import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { images } from "./db/schema";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
+  const user = auth();
 
-    const user = auth();
+  if (!user.userId) throw new Error("Unauthorized");
 
-    if (!user.userId) throw new Error("Unauthorized");
-
-    const images = await db.query.images.findMany({
-        where: (model, { eq }) => eq(model.userId, user.userId),
-        orderBy: (model, { desc }) => desc(model.id)
-    });
-    return images;
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return images;
 }
 
 export async function getImage(id: number) {
-    const user = auth();
+  const user = auth();
 
-    if (!user.userId) throw new Error("Unauthorized");
+  if (!user.userId) throw new Error("Unauthorized");
 
-    const image = await db.query.images.findFirst({
-        where: (model, { eq }) => eq(model.id, id)
-    });
+  const image = await db.query.images.findFirst({
+    where: (model, { eq }) => eq(model.id, id),
+  });
 
-    if (!image) throw new Error("Image not found");
+  if (!image) throw new Error("Image not found");
 
-    if (image.userId !== user.userId) throw new Error("Unauthorized");
+  if (image.userId !== user.userId) throw new Error("Unauthorized");
 
-    return image;
+  return image;
 }
 
 export async function deleteImage(id: number) {
-    const user = auth();
+  const user = auth();
 
-    if (!user.userId) throw new Error("Unauthorized");
+  if (!user.userId) throw new Error("Unauthorized");
 
-    await db.delete(images).where(and(eq(images.id, id), eq(images.userId, user.userId)))
+  await db
+    .delete(images)
+    .where(and(eq(images.id, id), eq(images.userId, user.userId)));
 
-    redirect("/");
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
+
+  redirect("/");
 }
